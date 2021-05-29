30eb8237c12ffed7fe5930eaa84bd8b9ff71f4a1
Phoenix-Robotics-Club <vishuvishnu1717@gmail.com>
2021-05-29 17:24:19 +0530

    first commit

---
+++ classify/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+import keras
+import os
+from keras.models import load_model
+import requests
+import urllib
+# Create your views here.
+def DeepModel(request):
+    result=""
+    test_input=''
+    model=load_model("static\cat_dog_model.h5")
+    if request.method == "POST":
+        try:
+            test_input=request.POST["input_url"]
+            new_image=Image.open(requests.get(test_input, stream=True).raw)
+            new_image=new_image.resize((64,64))
+            new_image=np.array(new_image)
+            predictions=model.predict(np.expand_dims(new_image, 0))
+            print(predictions)
+            if (predictions>0.5):
+                result="Dog"
+
+            else:
+                result="Cat"
+        except:
+            result="Image not loaded properly"
+        print(result)
+    return render(request,"home.html",context={'result':result,'result_img':test_input})
