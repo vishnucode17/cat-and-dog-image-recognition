2599a6fe5e921e5d2d67d26f072bfc6121d12522
vishnucode17 <61362200+vishnucode17@users.noreply.github.com>
2021-05-30 01:59:46 +0530

    Update settings.py

--- Cat_Dog_classification/settings.py
+++ Cat_Dog_classification/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=[
     STATIC_DIR,
 ]
